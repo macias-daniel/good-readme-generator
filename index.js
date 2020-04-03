512ce72993165b096105440d5ad291d7104f9c83
PinkFished <d.macias.apps@gmail.com>
2020-04-02 23:25:19 -0700

    Now prompts user and makes axios git call

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // TODO: import fs, path and inquirer modules
+const inquirer = require("inquirer")
+const axios = require("axios")
 
 // TODO: import api and generateMarkdown modules from ./utils/
 
@@ -10,7 +12,13 @@
 //   name: "github",
 //   message: "What is your GitHub username?"
 // }
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "username",
+        message: "What is your git hub username"
+    }
+];
 
 // TODO: Write function to synchronously write data in the
 // current working directory to file named for the fileName parameter.
@@ -23,7 +31,26 @@ function writeToFile(fileName, data) {
 // data from GitHub. Finally generate the markdown and use writeToFile
 // to create the README.md file.
 function init() {
+    inquirer.prompt(questions)
+    // inquirer callback
+    .then( answers => {
 
+        //Saving user username
+        const userName = answers.username
+
+        //Building user github api url
+        const gitUrl = `https://api.github.com/users/${userName}` 
+
+        return axios.get(gitUrl)
+
+    })
+    // axios callback
+    .then((githubResponse)=>{
+        console.log(githubResponse.data)
+    })
+    .catch(error =>{
+        console.log(error)
+    })
 }
 
 init();
